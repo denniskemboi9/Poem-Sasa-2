869c444bc8c5ca933f7df91ba6c0f44a2cbfe119
Dennis <cherogonydennis9@gmail.com>
2022-10-06 22:39:52 +0300

    Am using this template. I will work forwards from here

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
 
+//I'm also retaining this. it being the one to call APP.js and the first item to be run when code fires!
+
 ReactDOM.render(<App />, document.getElementById("root"));
